dd832fb21e9294c1dd3608a6eab5b1a61b8eed6e
sominparrrk <sompark98@gmail.com>
2024-01-15 21:09:40 +0000

    feat: add currency select component

---
+++ src/components/CurrencySelect/CurrencySelect.styles.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const StyledSelect = styled.select`
+  width: 200px;
+  height: 32px;
+  padding: 4px;
+  border-radius: 4px;
+  border: 1px solid ${({ theme }) => theme.color.gray};
+  color: ${({ theme }) => theme.color.black};
+  font-size: ${({ theme }) => theme.font.size.md};
+
+  &:focus {
+    border: 2px solid ${({ theme }) => theme.color.black};
+  }
+
+  &:active {
+    border: 2px solid ${({ theme }) => theme.color.black};
+  }
+
+  &::placeholder {
+    color: ${({ theme }) => theme.color.gray};
+  }
+`;
---
+++ src/components/CurrencySelect/CurrencySelect.tsx
@@ -0,0 +1,20 @@
+import { Currency } from "../../types/currency";
+import { StyledSelect } from "./CurrencySelect.styles";
+
+type CurrencySelectProps = {
+  options: Currency[];
+  value: string;
+  placeholder?: string;
+  onChange?: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+};
+
+export const CurrencySelect: React.FC<CurrencySelectProps> = ({ placeholder, value, onChange, options }) => {
+  return (
+    <StyledSelect value={value} onChange={onChange}>
+      {placeholder && <option value="">{placeholder}</option>}
+      {options.map((option: Currency, index: number) => (
+        <option key={index} value={option.code}>{option.code} - {option.name}</option>
+      ))}
+    </StyledSelect>
+  );
+};
