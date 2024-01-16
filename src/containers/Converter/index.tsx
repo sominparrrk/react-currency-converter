83d7c063cee815618c1a2bc544e8df7ffe46c643
sominparrrk <sompark98@gmail.com>
2024-01-16 05:38:20 +0000

    fix: set history to localStorage when there is no set toCurrency

--- src/containers/Converter/index.tsx
+++ src/containers/Converter/index.tsx
@@ -1,17 +1,19 @@
-import { useEffect, useState } from "react";
-import { ClearHistory, Container, FormWrapper, Text } from "./Converter.styles";
-import { Input } from "../../components/Input/Input";
-import { CurrencySelect } from "../../components/CurrencySelect/CurrencySelect";
-import { supportedCurrencies } from "../../lib/supportedCurrencies";
-import { HistoryList } from "../HistoryList";
-import getExchangeRate from "../../lib/api/getExchangeRate";
+import { useEffect, useState } from 'react';
+import { ClearHistory, Container, FormWrapper, Text } from './Converter.styles';
+import { Input } from '../../components/Input/Input';
+import { CurrencySelect } from '../../components/CurrencySelect/CurrencySelect';
+import { supportedCurrencies } from '../../lib/supportedCurrencies';
+import { HistoryList } from '../HistoryList';
+import getExchangeRate from '../../lib/api/getExchangeRate';
 
 export const Converter = () => {
   const [fromCurrency, setFromCurrency] = useState('GBP');
   const [toCurrency, setToCurrency] = useState('');
   const [fromAmount, setFromAmount] = useState(1);
   const [toAmount, setToAmount] = useState<number | null>(null);
-  const [exchangeRate, setExchangeRate] = useState<{ [key: string]: number }>({});
+  const [exchangeRate, setExchangeRate] = useState<{ [key: string]: number }>(
+    {}
+  );
   const [isSameCurrencyError, setIsSameCurrencyError] = useState(false);
   const [conversionHistory, setConversionHistory] = useState<string[]>([]);
 
@@ -26,11 +28,21 @@ export const Converter = () => {
   };
 
   useEffect(() => {
-    getExchangeRate(fromCurrency).then(rates => setExchangeRate(rates));
+    const localHistory = localStorage.getItem('history');
+    localHistory && setConversionHistory(JSON.parse(localHistory));
+  }, []);
+
+  useEffect(() => {
+    getExchangeRate(fromCurrency).then((rates) => setExchangeRate(rates));
   }, [fromCurrency]);
 
   useEffect(() => {
-    toCurrency && setConversionHistory([`${fromAmount} ${fromCurrency} 👉 ${toCurrency}`, ...conversionHistory]);
+    !isSameCurrencyError &&
+      toCurrency &&
+      setConversionHistory([
+        `${fromAmount} ${fromCurrency} 👉 ${toCurrency}`,
+        ...conversionHistory,
+      ]);
   }, [toAmount]);
 
   useEffect(() => {
@@ -38,28 +50,51 @@ export const Converter = () => {
   }, [fromCurrency, fromAmount, toCurrency, exchangeRate]);
 
   useEffect(() => {
-    const localHistory = localStorage.getItem('history');
-    localHistory && setConversionHistory(JSON.parse(localHistory));
-  }, []);
-
-  useEffect(() => {
-    toCurrency !== '' && localStorage.setItem('history', JSON.stringify(conversionHistory));
+    toCurrency !== '' &&
+      localStorage.setItem('history', JSON.stringify(conversionHistory));
   }, [conversionHistory]);
 
   return (
     <Container>
       <FormWrapper>
-        <Input value={fromAmount} onChange={e => setFromAmount(Number(e.target.value))} />
-        <CurrencySelect options={supportedCurrencies} value={fromCurrency} onChange={e => setFromCurrency(e.target.value)} />
+        <Input
+          value={fromAmount}
+          onChange={(e) => setFromAmount(Number(e.target.value))}
+        />
+        <CurrencySelect
+          options={supportedCurrencies}
+          value={fromCurrency}
+          onChange={(e) => setFromCurrency(e.target.value)}
+        />
       </FormWrapper>
       <Text $errorMsg={false}>equals</Text>
       <FormWrapper>
-        {toCurrency && toAmount && <Text $errorMsg={false}>{toAmount.toFixed(2)} {toCurrency}</Text>}
-        <CurrencySelect options={supportedCurrencies} placeholder="Select the currency" value={toCurrency} onChange={e => setToCurrency(e.target.value)} />
+        {toCurrency && toAmount && (
+          <Text $errorMsg={false}>
+            {toAmount.toFixed(2)} {toCurrency}
+          </Text>
+        )}
+        <CurrencySelect
+          options={supportedCurrencies}
+          placeholder="Select the currency"
+          value={toCurrency}
+          onChange={(e) => setToCurrency(e.target.value)}
+        />
       </FormWrapper>
-      {isSameCurrencyError && <Text $errorMsg={true}>Please select different currency</Text>}
-      {conversionHistory.length > 0 && <HistoryList history={conversionHistory} />}
-      <ClearHistory onClick={() => setConversionHistory([])}>Clear history</ClearHistory>
+      {isSameCurrencyError && (
+        <Text $errorMsg={true}>Please select different currency</Text>
+      )}
+      {conversionHistory.length > 0 && (
+        <HistoryList history={conversionHistory} />
+      )}
+      <ClearHistory
+        onClick={() => {
+          setConversionHistory([]);
+          localStorage.setItem('history', '');
+        }}
+      >
+        Clear history
+      </ClearHistory>
     </Container>
   );
 };
